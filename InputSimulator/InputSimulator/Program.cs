4246c76412ce979530238a6f42fd8780982601c5
Almir Abubekerov <kruiseabu@mail.ru>
2021-11-06 20:33:14 +0400

    added app

---
+++ InputSimulator/InputSimulator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using WindowsInput;
+
+namespace InputSimulatorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InputSimulator Simulator = new InputSimulator();
+
+            var str = "hello"; // File.ReadAllText(@"file path");
+
+            Simulator.Keyboard.Sleep(500).TextEntry(str);
+
+            Console.ReadKey();
+        }
+    }
+}
